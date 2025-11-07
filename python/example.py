724705dc6b7d3585bd620c5301c770940e397cf6
Lou DeGenaro <lou.degenaro@gmail.com>
2025-11-07 08:52:55 -0500

    add "bad" example
    Signed-off-by: Lou DeGenaro <lou.degenaro@gmail.com>

---
+++ python/example.py
@@ -0,0 +1,7 @@
+# Common IndexError trigger
+numbers = [1, 2, 3]
+print(numbers[4])  # IndexError: list index out of range
+
+# Empty list scenario
+empty_list = []
+print(empty_list[0])  # Another IndexError case
